1e6b1e910b4a3a37569788de7f494570ca969800
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2022-12-21 15:42:21 +0900

    ナビバー下部にダッシュボード項目を追加。

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   get 'pages/home'
+  get 'dashboard', to: 'users#dashboard'
+
+  post '/users/edit', to: 'users#update'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_20_001958) do
+ActiveRecord::Schema.define(version: 2022_12_21_062042) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2022_12_20_001958) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.text "about"
+    t.boolean "status", default: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
